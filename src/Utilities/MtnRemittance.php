9756d7274d3334f121e759a61c668e72810c5669
ottojoash <louisjoshbricks@gmail.com>
2022-09-21 02:17:59 -0400

    rem

---
+++ src/Utilities/MtnRemittance.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace ottojoash\MtnMomo;
+
+use ottojoash\MtnMomo\Exceptions\MtnMomoException;
+
+class MtnRemittance extends MtnMomo 
+{
+    const PRODUCT = 'remittance';
+
+
+    protected function requestUrl(string $endpointName, array $params = []) : string
+    { 
+        switch ($endpointName) {
+            case 'token':  
+                $urlSegment = '/remittance/token/' ;
+                break;
+
+            case 'createTransaction': 
+                $urlSegment = '/remittance/v1_0/transfer';
+                break;
+                 
+            case 'getTransaction': 
+                $urlSegment = "/remittance/v1_0/transfer/{$params['referenceId']}";
+                break;
+ 
+            case 'balance': 
+                $urlSegment = '/remittance/v1_0/account/balance';
+                break;
+ 
+            case 'accountholder':
+                $accountHolderIdType = $params['accountHolderIdType'] ?? 'MSISDN';
+                $urlSegment = "/remittance/v1_0/accountholder/{$accountHolderIdType}/{$params['accountHolderId']}/active";
+                break;
+            
+            default:
+                throw new MtnMomoException("Unknown api endpoint - {$endpointName}.");
+                break;
+        }
+ 
+        return $this->config->baseUrl . $urlSegment;
+    }
+
+    public function transfer(array $params, string $transactionUuid = null)
+    {
+        return parent::createTransaction($params, $transactionUuid);
+    }
+}
