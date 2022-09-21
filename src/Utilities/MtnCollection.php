4e8c06f5d2dbe29d1e478356888cac4601761e14
ottojoash <louisjoshbricks@gmail.com>
2022-09-21 01:55:56 -0400

    collection

---
+++ src/Utilities/MtnCollection.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace ottojoash\MtnMomo;
+
+use ottojoash\MtnMomo\Exceptions\MtnMomoException;
+
+class MtnCollection extends MtnMomo 
+{
+    const PRODUCT = 'collection';
+ 
+
+    protected function requestUrl(string $endpointName, array $params = []) : string
+    { 
+        switch ($endpointName) {
+            case 'token':  
+                $urlSegment = '/collection/token/' ; // trailing slash mandatory
+                break;
+
+            case 'createTransaction': 
+                $urlSegment = '/collection/v1_0/requesttopay';
+                break;
+                 
+            case 'getTransaction': 
+                $urlSegment = "/collection/v1_0/requesttopay/{$params['referenceId']}";
+                break;
+ 
+            case 'balance': 
+                $urlSegment = '/collection/v1_0/account/balance';
+                break;
+ 
+            case 'accountholderActive': 
+                $urlSegment = "/collection/v1_0/accountholder/MSISDN/{$params['accountHolderId']}/active";
+                break;
+            
+            default:
+                throw new MtnMomoException("Unknown Api endpoint - {$endpointName}.");
+                break;
+        }
+ 
+        return $this->config->baseUrl . $urlSegment;
+    }
+ 
+   
+    public function requestToPay(array $data, string $transactionUuid = null)
+    {
+        return parent::createTransaction($data, $transactionUuid);
+    }
+
+}
